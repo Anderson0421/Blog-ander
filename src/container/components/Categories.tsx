9044c7b8a3686b1fd1fcc0c19ec12729304674c2
Anderson0421 <jelow0413@gmail.com>
2024-04-03 16:30:52 -0500

    Refactor Categories component styling

--- src/container/components/Categories.tsx
+++ src/container/components/Categories.tsx
@@ -14,7 +14,6 @@ export default function Categories() {
             const response = await fetch('https://backend-django-blog.onrender.com/api/v1/list/categories')
             const data = await response.json()
             setCategories(data)
-            console.log(data)
         }
         GetData()
     }, [])
@@ -29,12 +28,12 @@ export default function Categories() {
 
     return (
         <>
-            <div className="text-white text-sm mt-5 mb-2 flex gap-2 ">
+            <div className="text-white max-sm:flex-wrap text-sm mt-5 mb-2 flex gap-2 ">
                 {
                     categories ? categories.map((category, index) => {
                         return (
                             <Link onClick={() => handleCategoryClick(category)} key={index} to={`/categories/${category.slug}`}
-                                className='px-32 flex text-gray-300 rounded-sm hover:bg-gray-800 duration-300 transition-all justify-center border border-gray-500 py-3 w-32'
+                                className='px-32 max-sm:w-full flex text-gray-300 rounded-sm hover:bg-gray-800 duration-300 transition-all justify-center border border-gray-500 py-3 w-32'
                             >
                                 <span>
                                     {category.name}
